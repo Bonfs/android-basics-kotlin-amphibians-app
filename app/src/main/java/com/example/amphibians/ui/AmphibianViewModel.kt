a34fc10d555947406115874ba692f6a5a5e7f5b6
bonfs <matheusbonfim05@gmail.com>
2022-04-02 19:11:41 -0300

    remoção de TODOS

--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -15,7 +15,6 @@
  */
 package com.example.amphibians.ui
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -23,27 +22,21 @@ import androidx.lifecycle.viewModelScope
 import com.example.amphibians.data.Amphibian
 import com.example.amphibians.network.AmphibianApi
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
 enum class AmphibianApiStatus { LOADING, ERROR, DONE }
 
 class AmphibianViewModel : ViewModel() {
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for the API status
     private val _status = MutableLiveData<AmphibianApiStatus>()
     val status: LiveData<AmphibianApiStatus> = _status
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a list of amphibian objects
     private val _amphibians = MutableLiveData<List<Amphibian>>()
     val amphibians: LiveData<List<Amphibian>> = _amphibians
 
-    // TODO: Create properties to represent MutableLiveData and LiveData for a single amphibian object.
     //  This will be used to display the details of an amphibian when a list item is clicked
     private val _amphibian = MutableLiveData<Amphibian>()
     val amphibian: LiveData<Amphibian> = _amphibian
 
-    // TODO: Create a function that gets a list of amphibians from the api service and sets the
-    //  status via a Coroutine
     fun getAmphibianList() {
         _status.value = AmphibianApiStatus.LOADING
         viewModelScope.launch {
@@ -52,7 +45,6 @@ class AmphibianViewModel : ViewModel() {
                 _status.value = AmphibianApiStatus.DONE
             } catch (e: Exception) {
                 e.printStackTrace()
-                //Log.d("ViewModel", e.toString())
                 _status.value = AmphibianApiStatus.ERROR
                 _amphibians.value = listOf()
             }
@@ -60,7 +52,6 @@ class AmphibianViewModel : ViewModel() {
     }
 
     fun onAmphibianClicked(amphibian: Amphibian) {
-        // TODO: Set the amphibian object
         _amphibian.value = amphibian
     }
 }
